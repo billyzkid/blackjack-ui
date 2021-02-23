fbba20bd4bf87fe2a818985d3cfea9021b380343
Will Allan <will.allan@disneystreaming.com>
2021-02-22 23:24:17 -0800

    Menu stuff

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ $(document).ready(function() {
     $.fn.popup.defaults.pagecontainer = '.table';
     $.fn.popup.defaults.transition = 'all 0.3s';
 
-    $('#info-popup-content').popup();
-    $('#user-popup-content').popup();
-    $('#chat-popup-content').popup();
-    $('#sound-popup-content').popup();
-    $('#quit-popup-content').popup();
+    $('#info-popup-content').popup({ onopen: () => $('.info-popup-content_open').addClass('active'), onclose: () => $('.info-popup-content_open').removeClass('active') });
+    $('#user-popup-content').popup({ onopen: () => $('.user-popup-content_open').addClass('active'), onclose: () => $('.user-popup-content_open').removeClass('active') });
+    $('#chat-popup-content').popup({ onopen: () => $('.chat-popup-content_open').addClass('active'), onclose: () => $('.chat-popup-content_open').removeClass('active') });
+    $('#sound-popup-content').popup({ onopen: () => $('.sound-popup-content_open').addClass('active'), onclose: () => $('.sound-popup-content_open').removeClass('active') });
+    $('#quit-popup-content').popup({ onopen: () => $('.quit-popup-content_open').addClass('active'), onclose: () => $('.quit-popup-content_open').removeClass('active') });
 
     function createDeck() {
         const deck = [];
