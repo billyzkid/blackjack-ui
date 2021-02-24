71e009dee2c24f08674f34e62a77cc2de8d82249
Will Allan <will.allan@disneystreaming.com>
2021-02-23 20:14:11 -0800

    Popups

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ $(document).ready(function() {
     $.fn.popup.defaults.pagecontainer = '.table';
     $.fn.popup.defaults.transition = 'all 0.3s';
 
-    $('#info-popup-content').popup({ onopen: () => $('.info-popup-content_open').addClass('active'), onclose: () => $('.info-popup-content_open').removeClass('active') });
-    $('#user-popup-content').popup({ onopen: () => $('.user-popup-content_open').addClass('active'), onclose: () => $('.user-popup-content_open').removeClass('active') });
-    $('#chat-popup-content').popup({ onopen: () => $('.chat-popup-content_open').addClass('active'), onclose: () => $('.chat-popup-content_open').removeClass('active') });
-    $('#sound-popup-content').popup({ onopen: () => $('.sound-popup-content_open').addClass('active'), onclose: () => $('.sound-popup-content_open').removeClass('active') });
-    $('#quit-popup-content').popup({ onopen: () => $('.quit-popup-content_open').addClass('active'), onclose: () => $('.quit-popup-content_open').removeClass('active') });
+    $('#info-popup-content').popup({ openelement: '#info-menu-item', onopen: () => $('#info-menu-item').addClass('active'), onclose: () => $('#info-menu-item').removeClass('active') });
+    $('#user-popup-content').popup({ openelement: '#user-menu-item', onopen: () => $('#user-menu-item').addClass('active'), onclose: () => $('#user-menu-item').removeClass('active') });
+    $('#chat-popup-content').popup({ openelement: '#chat-menu-item', onopen: () => $('#chat-menu-item').addClass('active'), onclose: () => $('#chat-menu-item').removeClass('active') });
+    $('#sound-popup-content').popup({ openelement: '#sound-menu-item', onopen: () => $('#sound-menu-item').addClass('active'), onclose: () => $('#sound-menu-item').removeClass('active') });
+    $('#quit-popup-content').popup({ openelement: '#quit-menu-item', onopen: () => $('#quit-menu-item').addClass('active'), onclose: () => $('#quit-menu-item').removeClass('active') });
 
     function createDeck() {
         const deck = [];
